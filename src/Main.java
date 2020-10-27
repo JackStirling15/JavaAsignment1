fb586191919b582fb1ce1a0e7bb0e2527fdc0adc
jack <jack.stirling@bt.com>
2020-10-27 14:41:28 +0000

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main {
+import uk.ac.uos.i2p.assignment.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        ContactTracing CTinstance = new ContactTracingImpl();
+        Map<String, String> studentEntry = new HashMap<>();
+        studentEntry.put("Jack", "s210887");
+        studentEntry.put("Dave", "s5429384");
+        CTinstance.loadCourseList(studentEntry);
+
+        Map<String, String> studentEmail = new HashMap<>();
+
     }
 }
---
+++ src/uk/ac/uos/i2p/assignment/ContactTracingImpl.java
@@ -0,0 +1,59 @@
+package uk.ac.uos.i2p.assignment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ContactTracingImpl implements ContactTracing {
+    private Map<String, String> studentList;
+    private Map<String, String> emailList;
+    private Map<String, String> courseList;
+    private Map<String, String> studentCourseList;
+
+    public void loadStudentList(Map<String, String> studentEntry){
+        studentList = studentEntry;
+    }
+    public void loadCourseList(Map<String, String> courseEntry){
+        courseList = courseEntry;
+    }
+    public void loadEmailList(Map<String, String> emailEntry){
+        emailList = emailEntry;
+    }
+    public void loadStudentCourseList(Map<String, String> studentCourseEntry){
+        studentCourseList = studentCourseEntry;
+    }
+    public List<String> findMatchingCourses(String inputStudentNumber){
+        List<String> selectedCourseList = new ArrayList();
+        studentCourseList.forEach((studentID, linkedCourseID)->{
+            if (studentID.equals(inputStudentNumber)){
+                courseList.forEach((courseName, id)->{
+                    if (linkedCourseID.equals(id)){
+                        selectedCourseList.add(courseName);
+                    }
+                });
+            }
+        });
+
+        return selectedCourseList;
+    }
+    public List<String> findMatchingStudents(List<String> matchingCourseList){
+        List<String> exposedStudents = new ArrayList();
+        matchingCourseList.forEach((course)->{
+            studentCourseList.forEach((studentID, courseID)->{
+                if (course.equals(courseID)){
+                    exposedStudents.add(studentID);
+                }
+            });
+        });
+        return exposedStudents;
+    }
+
+    public List<String> contactTracing(String inputStudentNumber){
+        List<String> emails = new ArrayList();
+
+        List<String> courseList = findMatchingCourses(inputStudentNumber);
+        List<String> studentList = findMatchingStudents(courseList);
+        studentList.forEach((studentID)-> emails.add(emailList.get(studentID)));
+        return emails;
+    }
+}
